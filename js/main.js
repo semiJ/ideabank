ba621e00ae9b0cddedbbf5a25777a652da06f71f
semiJ <04tpal04@gmail.com>
2024-11-06 16:13:29 +0900

    개인홈페이지 업데이트

--- js/main.js
+++ js/main.js
@@ -15,6 +15,30 @@ $(function(){
         });
     });
 
+    //aboutme
+
+    $(window).on("scroll", function(){
+        let abou = $(this).scrollTop();
+        console.log(abou); // 스크롤 위치 확인
+
+        if(abou >= 200 && abou < 1600) {
+            $(".myInfo").addClass("infoview");
+        } else {
+            $(".myInfo").removeClass("infoview");
+        };
+    });
+
+    $(window).on("scroll", function(){
+        let abou = $(this).scrollTop();
+        // console.log(abou); // 스크롤 위치 확인
+
+        if(abou >= 200 && abou < 1600) {
+            $(".diligent").addClass("diligentani");
+        } else {
+            $(".diligent").removeClass("diligentani");
+        };
+    });
+
     // bi 노출
 
     $(window).on("scroll", function(){
@@ -28,8 +52,64 @@ $(function(){
         };
     });
 
-    //coding 영역 자리 잡기
+    $(window).on("scroll", function(){
+        let viewimg = $(this).scrollTop();
+        // console.log(viewimg); // 스크롤 위치 확인
 
+        if(viewimg >= 1200 && viewimg < 2600) {
+            $(".bi_img_main").addClass("imgview");
+        } else {
+            $(".bi_img_main").removeClass("imgview");
+        };
+    });
+
+    //coding 영역
+    // 숫자 클릭 시 해당하는 포폴 보이기
+
+    $(window).on("scroll", function(){
+
+        let pofolTop = $(this).scrollTop();
+        console.log(pofolTop);
+
+        if(pofolTop >= 2150) {
+            $(".portfolio .pofol_one").addClass("pofol_active");   
+        } else {
+            $(".portfolio .pofol_one").removeClass("pofol_active");
+        };
+
+        if(pofolTop >= 2350) {
+            $(".portfolio .pofol_tow").addClass("pofol_active");   
+        } else {
+            $(".portfolio .pofol_tow").removeClass("pofol_active");
+        };
+
+        if(pofolTop >= 2550) {
+            $(".portfolio .pofol_three").addClass("pofol_active");   
+        } else {
+            $(".portfolio .pofol_three").removeClass("pofol_active");
+        };
+
+        if(pofolTop >= 2750) {
+            $(".portfolio .pofol_four").addClass("pofol_active");   
+        } else {
+            $(".portfolio .pofol_four").removeClass("pofol_active");
+        };
+
+    });
+
+    $(".portfolio .pofol_num li").on("click", function(){
+        i = $(this).index();
+
+        $(".portfolio .pofol").removeClass("pofol_active")
+        $(".portfolio .pofol").eq(i).addClass("pofol_active")
+    });
+
+    $().on("clik", function(){
+        i = $(this).index();
+
+        $(".portfolio .pofol").removeClass("pofol_active")
+        $(".portfolio .pofol").eq(i).addClass("pofol_active")
+    })
 
 
     // design 영역 벌집 자리 잡기
@@ -39,43 +119,43 @@ $(function(){
         let sixTop = $(this).scrollTop();
         // console.log(sixTop);
 
-        if(sixTop >= 2700) {
+        if(sixTop >= 2800) {
             $(".hexagon").addClass("dsgview");   
         } else {
             $(".hexagon").removeClass("dsgview");
         };
 
-        if(sixTop >= 2800) {
+        if(sixTop >= 2900) {
             $(".hexagon2").addClass("dsgview");   
         } else {
             $(".hexagon2").removeClass("dsgview");
         };
 
-        if(sixTop >= 2900) {
+        if(sixTop >= 3000) {
             $(".hexagon3").addClass("dsgview");   
         } else {
             $(".hexagon3").removeClass("dsgview");
         };
 
-        if(sixTop >= 3000) {
+        if(sixTop >= 3100) {
             $(".hexagon4").addClass("dsgview");   
         } else {
             $(".hexagon4").removeClass("dsgview");
         };
 
-        if(sixTop >= 3100) {
+        if(sixTop >= 3200) {
             $(".hexagon5").addClass("dsgview");   
         } else {
             $(".hexagon5").removeClass("dsgview");
         };
 
-        if(sixTop >= 3200) {
+        if(sixTop >= 3300) {
             $(".hexagon6").addClass("dsgview");   
         } else {
             $(".hexagon6").removeClass("dsgview");
         };
 
-        if(sixTop >= 3300) {
+        if(sixTop >= 3400) {
             $(".hexagon7").addClass("dsgview");   
         } else {
             $(".hexagon7").removeClass("dsgview");
